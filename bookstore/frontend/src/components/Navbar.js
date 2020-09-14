587521634b645df7679e82f2b355628bc1e204b7
zeevNoiman <zevnoiman@gmail.com>
2020-09-14 13:32:31 +0300

    change fonts

--- bookstore/frontend/src/components/Navbar.js
+++ bookstore/frontend/src/components/Navbar.js
@@ -64,7 +64,6 @@ export default function Navbar() {
                                         </h3>
                                     </li>
                                 }
-
                             </ul>
                             {!isAuthenticated ? (
                                 <ul className="navbar-nav-actions">
